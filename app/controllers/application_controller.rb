007d249d2399870d6bca82d2b83e37486b3e6d99
KouSei089 <qq00891829@icloud.com>
2021-06-16 08:13:40 +0900

    login_top

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :success, :info, :warning, :danger
   before_action :require_login
   before_action :set_search
 
   def set_search
     @q = Post.ransack(params[:q])
     @posts = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
+  end
+
+  def not_authenticated
+    redirect_to login_url
+    flash[:danger] = 'Please login. or Create Account'
   end
 end
