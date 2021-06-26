ac03258a2339fc5b3efdb377727e524e4e2cf75c
KouSei089 <qq00891829@icloud.com>
2021-06-26 23:19:06 +0900

    Add:users_spec

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  before do
-    driven_by(:rack_test)
+  describe 'ログイン前' do
+    it "アカウント登録" do
+      visit sign_up_path
+      fill_in 'Email', with: 'user_1@example.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password confirmation', with: 'password'
+      click_button 'SignUp'
+      expect(current_path).to eq login_path
+      expect(page).to have_content 'User was successfully created.'
+    end
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
 end
