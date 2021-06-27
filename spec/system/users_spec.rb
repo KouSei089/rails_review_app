d1f3279056876cb4d91eb769de21b7adf82c4302
KouSei089 <qq00891829@icloud.com>
2021-06-27 22:44:25 +0900

    Add:Post_spec

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe "Users", type: :system do
       expect(page).to have_content 'User was successfully created.'
     end
   end
+  describe 'Task削除' do
+    context '正常系' do
+      it 'Taskが削除されること' do
+        visit project_tasks_path(project)
+        click_link 'Destroy'
+        page.driver.browser.switch_to.alert.accept
+        expect(page).not_to have_content task.title
+        expect(Task.count).to eq 0
+        expect(current_path).to eq project_tasks_path(project)
+      end
+    end
+  end
 end
